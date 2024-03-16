5aa187d3d88159e2cea4e67115cc8e73a4717ca0
Bharath Babu <bharathbabu68@gmail.com>
2024-03-16 17:02:59 +0000

    feat: Added worldcoin connect package

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,43 @@
 import logo from './logo.svg';
 import './App.css';
+import { IDKitWidget, VerificationLevel, ISuccessResult } from '@worldcoin/idkit'
 
 function App() {
+
+  const handleVerify = async (proof: ISuccessResult) => {
+    const res = await fetch("/api/verify", { // route to your backend will depend on implementation
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify(proof),
+    })
+    if (!res.ok) {
+        throw new Error("Verification failed."); // IDKit will display the error message to the user in the modal
+    }
+};
+
+const onSuccess = () => {
+  // This is where you should perform any actions after the modal is closed
+  // Such as redirecting the user to a new page
+  window.location.href = "/success";
+};
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <IDKitWidget
+	app_id={process.env.REACT_APP_WORLDCOIN_APP_ID} // obtained from the Developer Portal
+	action={process.env.REACT_APP_WOLDCOIN_ACTION_ID} // obtained from the Developer Portal
+	onSuccess={onSuccess} // callback when the modal is closed
+	handleVerify={handleVerify} // callback when the proof is received
+	verification_level={VerificationLevel.Device}
+>
+	{({ open }) => 
+        // This is the button that will open the IDKit modal
+        <button onClick={open}>Verify with World ID</button>
+    }
+</IDKitWidget>
     </div>
   );
 }
